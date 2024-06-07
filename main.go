ccfd8f6f202e65161d84be9be163fa89a9b45aab
ysayranb <sayranbekov.0000@gmail.com>
2024-06-08 01:09:19 +0600

    Responsibilities

--- main.go
+++ main.go
@@ -12,38 +12,20 @@ const (
 	BLUE   = "\033[34m"
 	YELLOW = "\033[33m"
 )
-
+// Adi
 func main() {
 	var HEIGHT int
 	var WIDTH int
 
 	fmt.Scanf("%d", &HEIGHT)
 	fmt.Scanf("%d", &WIDTH)
-	printMap(HEIGHT, WIDTH)
 }
 
+// Azat 
 func printCell(value rune) {
 	for i := 0; i < 7; i++ {
 		ap.PutRune(value)
 	}
 }
 
-func printMap(height int, width int) {
-	// Loop to iterate over every row.
-	// Multiplied by 3 because every row has 3 rows inside.
-	// And additional 1 to print first line
-	for h := 0; h < (3*height)+1; h++ {
-		// Loop to iterate over every column
-
-		for w := 0; w < (7*width)+width+1; w++ {
-			if w == 0 && h != 0 || w%8 == 0 && h != 0 {
-				fmt.Print("|")
-			} else if h == 0 || h%3 == 0 {
-				fmt.Print("_")
-			} else {
-				fmt.Print(" ")
-			}
-		}
-		fmt.Print("\n")
-	}
-}
+// Additional tasks Yerkanat

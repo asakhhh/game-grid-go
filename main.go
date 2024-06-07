852a4ff29378fd9dbacaf0fee4bf61d45f9b34b3
aakash <azat.akash@nu.edu.kz>
2024-06-08 01:43:48 +0600

    bssic operations

--- main.go
+++ main.go
@@ -21,13 +21,80 @@ func main() {
 
 	fmt.Scanf("%d", &HEIGHT)
 	fmt.Scanf("%d", &WIDTH)
+	printCell(WIDTH)
+}
+
+func printFullRow(value rune) {
+	for i := 0; i < 7; i++ {
+		if i == 0 {
+			ap.PutRune('|')
+		} else {
+			ap.PutRune(value)
+		}
+	}
+}
+
+func printRuneAtCentre(value rune) {
+	for i := 0; i < 7; i++ {
+		if i == 0 {
+			ap.PutRune('|')
+		} else if i == 3 {
+			ap.PutRune(value)
+		} else {
+			ap.PutRune(' ')
+		}
+	}
 }
 
 // Azat
-func printCell(value rune) {
-	for i := 0; i < 7; i++ {
-		ap.PutRune(value)
-		fmt.Print("Hello")
+func printCell(value int) {
+	ap.PutRune(' ')
+	// for i := 0; i < 7; i++ {
+	// 	ap.PutRune('_')
+	// }
+	ap.PutRune('\n')
+
+	if value == 0 {
+		for i := 0; i < 3; i++ {
+
+			printFullRow('X')
+
+			ap.PutRune('\n')
+		}
+	} else if value == 1 {
+		for i := 0; i < 3; i++ {
+
+			if i == 2 {
+				printFullRow('_')
+			} else {
+				printFullRow(' ')
+			}
+			ap.PutRune('\n')
+		}
+	} else if value == 2 {
+		for i := 0; i < 3; i++ {
+
+			if i == 1 {
+				printRuneAtCentre('>')
+			} else if i == 2 {
+				printFullRow('_')
+			} else {
+				printFullRow(' ')
+			}
+			ap.PutRune('\n')
+		}
+	} else if value == 3 {
+		for i := 0; i < 3; i++ {
+
+			if i == 1 {
+				printRuneAtCentre('@')
+			} else if i == 2 {
+				printFullRow('_')
+			} else {
+				printFullRow(' ')
+			}
+			ap.PutRune('\n')
+		}
 	}
 }
 

ca34c969b43acd93d021b447bbfb0be5094dd22f
Yerkanat <sayranbekov.0000@gmail.com>
2024-06-08 16:50:46 +0600

    Replace fmt.Print to ap.PutRune

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/alem-platform/ap"
+)
 
 var (
-	RESET  = "\033[0m"
-	RED    = "\033[31m"
-	WHITE  = "\033[97m"
-	BLUE   = "\033[34m"
-	YELLOW = "\033[33m"
+	RESET  = []rune{'\033', '[', '0', 'm'}
+	RED    = []rune{'\033', '[', '3', '1', 'm'}
+	WHITE  = []rune{'\033', '[', '9', '7', 'm'}
+	BLUE   = []rune{'\033', '[', '3', '4', 'm'}
+	YELLOW = []rune{'\033', '[', '3', '3', 'm'}
 )
 
 var (
@@ -36,31 +40,69 @@ func main() {
 	printMap(HEIGHT, WIDTH, full_map)
 }
 
+func colorize(val int) {
+	if val == 0 {
+		for _, r := range RED {
+			ap.PutRune(r)
+		}
+	} else if val == 2 {
+		for _, r := range BLUE {
+			ap.PutRune(r)
+		}
+	} else if val == 3 {
+		for _, r := range YELLOW {
+			ap.PutRune(r)
+		}
+	} else if val == 4 {
+		for _, r := range RESET {
+			ap.PutRune(r)
+		}
+	}
+}
+
 // printCell function prints a single cell based on its value
 func printCell(value int, x int, y int) {
 	if value == 0 {
-		fmt.Print(RED + string(wall) + RESET) // Wall
+		colorize(value)
+		ap.PutRune(wall)
+		colorize(4)
 	} else if value == 1 {
 		if x%3 == 0 {
-			fmt.Print("_") // Horizontal separator
+			colorize(value)
+			ap.PutRune('_')
+			colorize(4)
 		} else {
-			fmt.Print(" ") // Empty space
+			colorize(value)
+			ap.PutRune(' ')
+			colorize(4)
 		}
 	} else if value == 2 {
 		if x == 2 && y == 4 {
-			fmt.Print(BLUE + string(player) + RESET) // Player
+			colorize(value)
+			ap.PutRune(player)
+			colorize(4)
 		} else if x == 0 {
-			fmt.Print("_") // Horizontal separator
+			colorize(value)
+			ap.PutRune(' ')
+			colorize(4)
 		} else {
-			fmt.Print(" ") // Empty space
+			colorize(value)
+			ap.PutRune(' ')
+			colorize(4)
 		}
 	} else if value == 3 {
 		if x == 2 && y == 4 {
-			fmt.Print(YELLOW + string(award) + RESET) // Award
+			colorize(value)
+			ap.PutRune(award)
+			colorize(4)
 		} else if x == 0 {
-			fmt.Print("_") // Horizontal separator
+			colorize(value)
+			ap.PutRune('_')
+			colorize(4)
 		} else {
-			fmt.Print(" ") // Empty space
+			colorize(value)
+			ap.PutRune(' ')
+			colorize(4)
 		}
 	}
 }
@@ -72,9 +114,11 @@ func printMap(height int, width int, value [][]int) {
 	for h := 0; h < (3*height)+1; h++ {
 		// Print vertical coordinates
 		if (h+1)%3 == 0 && h != 0 {
-			fmt.Printf("%2d", (h+1)/3) // Print vertical coordinate every 3rd row
+			ap.PutRune(' ')
+			ap.PutRune(rune(((h + 1) / 3) + '0'))
 		} else {
-			fmt.Print("  ") // Print space for alignment
+			ap.PutRune(' ')
+			ap.PutRune(' ')
 		}
 
 		// Loop to iterate over every column
@@ -82,28 +126,29 @@ func printMap(height int, width int, value [][]int) {
 		if h == 0 {
 			for w := 0; w < (8*width)+1; w++ {
 				if (w+3)%7 == 0 && w != 0 {
-					fmt.Print(string(rune(64 + (w+3)/7))) // Print horizontal coordinates
+					ap.PutRune(rune(64 + (w+3)/7)) // Print horizontal coordinates
 				} else {
-					fmt.Print(" ") // Print space for alignment
+					ap.PutRune(' ')
 				}
 			}
-			fmt.Print("\n") // Move to the next line after printing coordinates
+			ap.PutRune('\n')
 		}
 
 		for w := 0; w < (8*width)+1; w++ {
 			if w == 0 && h != 0 || w%8 == 0 && h != 0 {
-				fmt.Print("|") // Vertical border
+				ap.PutRune('|')
 			} else if h == 0 {
 				if w == 0 {
-					fmt.Print("  ")
+					ap.PutRune(' ')
+					ap.PutRune(' ')
 				}
-				fmt.Print("_") // Horizontal border
+				ap.PutRune('_')
 			} else if h%3 != 0 {
 				printCell(value[h/3][w/8], h%3, w%8) // Print cell content
 			} else {
 				printCell(value[h/3-1][w/8], h%3, w%8) // Print cell content
 			}
 		}
-		fmt.Print("\n") // Move to the next line after printing a row
+		ap.PutRune('\n')
 	}
 }
